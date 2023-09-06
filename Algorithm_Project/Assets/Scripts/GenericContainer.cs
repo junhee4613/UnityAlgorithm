7020db6270837a6948a5ffbc248c8c94c9c27665
junhee4613 <junhee000123@naver.com>
2023-09-06 15:16:34 +0900

    Feat: "제네릭 테스트 구현"
    커스텀한 배열을 만들고 키보드 연결해서 동작 테스트

---
+++ Algorithm_Project/Assets/Scripts/GenericContainer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericContainer<T>
+{
+    private T[] items;
+    private int currentIndex = 0;
+    public GenericContainer(int capcity)
+    {
+        items = new T[capcity];
+    }
+
+    public void Add(T item)
+    {
+        if(currentIndex < items.Length)
+        {
+            items[currentIndex] = item;
+            currentIndex++;
+        }
+    }
+
+    public T[] GetItems()
+    {
+        return items;
+    }
+}
